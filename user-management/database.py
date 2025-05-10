047b7738d4e75f3ffee642f136ad80ab9ab65521
KalinduBihan <klindubihanfdo@gmail.com>
2025-05-10 22:01:05 +0530

    commit 03

--- user-management/database.py
+++ user-management/database.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 client = pymongo.MongoClient(MONGO_URI)
-db = client["auth_db"]  # Database name
+db = client["auth_db"]
 users_collection = db["users"]
