3aa32ec2ae1380418714e2beb9c275e543244214
LasenHewage <lasenhewage111@gnail.com>
2025-05-10 15:17:52 +0530

    order_management

---
+++ order-management/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import uvicorn
+from order import router as order_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- You can change this to ["http://localhost:3000"] etc.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(order_router, prefix="/orders", tags=["Orders"])
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the authentication microservice"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8060)
